cdaf01e38b6c1e0966b98accd794e378dd01836b
Michele Alda <MAlda@binck.nl>
2019-02-07 09:41:01 +0100

    Project renamed

--- InstrumentQuote.cs
+++ InstrumentQuote.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace StreamerClientExample
+﻿namespace ClientCsharp
 {
+    using System;
+    using System.Collections.Generic;
+
     public class InstrumentQuote
     {
         public string Id { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
-
-namespace StreamerClientExample
+﻿namespace ClientCsharp
 {
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.SignalR.Client;
+    using Microsoft.Extensions.Logging;
+
     public class Program
     {
         private const string AEXIndex = "eM1X1";
